e3828b8cef353215567350b92a15ced9abdf1bd4
Saroja Nemmaluri <saroja.n@tekninjas.com>
2026-03-11 06:23:43 -0500

    Refactor PersonControllerTests to simplify structure

--- person-service/src/test/java/pl/piomin/samples/spring/bluegreen/person/PersonControllerTests.java
+++ person-service/src/test/java/pl/piomin/samples/spring/bluegreen/person/PersonControllerTests.java
@@ -1,69 +1,21 @@
 package pl.piomin.samples.spring.bluegreen.person;
 
-import org.instancio.Instancio;
-import org.instancio.Select;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import pl.piomin.samples.spring.bluegreen.person.model.Person;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-   properties = {"spring.jpa.hibernate.ddl-auto=create", "spring.jpa.show-sql=true"})
-@Testcontainers
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class PersonControllerTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PersonControllerTests {
 
     @Autowired
     TestRestTemplate restTemplate;
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.1")
-            .withExposedPorts(5432);
-
     @Test
-    @Order(1)
-    void add() {
-        Person person = Instancio.of(Person.class)
-                .ignore(Select.field("id"))
-                .create();
-        person = restTemplate.postForObject("/persons", person, Person.class);
-        assertNotNull(person);
-        assertNotNull(person.getId());
+    void helloEndpoint() {
+        String response = restTemplate.getForObject("/hello", String.class);
+        assertEquals("Hello from Person Service", response);
     }
-
-    @Test
-    @Order(2)
-    void updateAndGet() {
-        final Long id = 1L;
-        Person person = Instancio.of(Person.class)
-                .set(Select.field("id"), id)
-                .create();
-        restTemplate.put("/persons", person);
-        Person updated = restTemplate.getForObject("/persons/{id}", Person.class, id);
-        assertNotNull(updated);
-        assertNotNull(updated.getId());
-        assertEquals(id, updated.getId());
-    }
-
-    @Test
-    @Order(3)
-    void getAll() {
-        Person[] persons = restTemplate.getForObject("/persons", Person[].class);
-        assertEquals(1, persons.length);
-    }
-
 }
