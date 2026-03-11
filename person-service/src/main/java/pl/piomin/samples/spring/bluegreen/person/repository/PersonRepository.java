2fe1ebdd9bf3c768fcef6ae8d158b282c45b3558
Saroja Nemmaluri <saroja.n@tekninjas.com>
2026-03-11 05:44:05 -0500

    Delete person-service/src/main/java/pl/piomin/samples/spring/bluegreen/person/repository directory

--- person-service/src/main/java/pl/piomin/samples/spring/bluegreen/person/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.piomin.samples.spring.bluegreen.person.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.piomin.samples.spring.bluegreen.person.model.Person;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
